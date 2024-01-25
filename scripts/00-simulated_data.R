17c9c0c5ddaf76d288e194385ed894107b54432e
Elaineyi1 <boxuan.yi@mail.utoronto.ca>
2024-01-25 03:00:19 +0000

    Add some descriptions for simulations

--- scripts/00-simulated_data.R
+++ scripts/00-simulated_data.R
@@ -1,7 +1,7 @@
 # Purpose: Simulate the dataset of major crimes in Toronto
 # Author: Boxuan Yi
 # Email: boxuan.yi@mail.utoronto.ca
-# Date: 17 January 2024
+# Date: 22 January 2024
 # There are four simulations
 
 # Install all the packages I need
@@ -22,8 +22,10 @@ library(knitr)
 cat("![My plan](project/scripts/plan.png)\n")
 
 # Simulate
-# Simulation for the number of crimes in each year (2014-2023),
-# and the month with the most crimes in each year
+# Simulation for the number of crimes in each year (2014-2023), and the month with the most crimes in each year
+# It gives a table with 10 rows, and 3 variables: Year (2014-2023), Number of Crimes and Month with the most crimes each year.
+# The number of crimes is randomly chosen using uniform distribution from 0 to 10000.
+# Month is randomly chosen 10 times with replacement
 set.seed(7)
 months <- c("January", "February", "March", "April", "May", "June", "July",
             "August", "September", "October", "November", "December")
@@ -31,11 +33,13 @@ simulated_data1 <-
   tibble(
     "Year" = 2014:2023,
     "Number of Crimes" = runif(n = 10, min = 0, max = 10000),
-    "Month" = sample(months, 10, replace = TRUE)
-  )
+    "Month with the most crimes" = sample(months, 10, replace = TRUE)
+  ) |> kable()
 simulated_data1
 
 # Simulation for the number of crimes classified in major crime types
+# It gives a table with years (2014 to 2023) and numbers of crimes for 5 types of crime
+# Numbers of crimes are randomly chosen using uniform distribution from 0 to 2000.
 set.seed(7)
 simulated_data2 <-
   tibble(
@@ -45,10 +49,12 @@ simulated_data2 <-
     "Number of Break and Enter" = runif(n = 10, min = 0, max = 2000),
     "Number of Robbery" = runif(n = 10, min = 0, max = 2000),
     "Number of Theft Over" = runif(n = 10, min = 0, max = 2000)
-  )
+  ) |> kable()
 simulated_data2
 
 # Simulation for the number of crimes classified in major crime types
+# It gives a table with years (2014 to 2023) and numbers of crimes for 6 different regions
+# Numbers of crimes are randomly chosen using uniform distribution from 0 to 1000.
 set.seed(7)
 simulated_data3 <-
   tibble(
@@ -59,14 +65,16 @@ simulated_data3 <-
     "Other" = runif(n = 10, min = 0, max = 1000),
     "Scarborough" = runif(n = 10, min = 0, max = 1000),
     "Toronto-East York" = runif(n = 10, min = 0, max = 1000)
-  )
+  ) |> kable()
 simulated_data3
 
 # Simulation for the number of crimes classified in major crime types
+# It gives a table with years (2014 to 2023) and numbers of crimes in 24 hours
+# Numbers of crimes are randomly chosen using uniform distribution from 0 to 1000.
 set.seed(7)
 simulated_data4 <-
   tibble(
     "Hour" = 0:23,
-    "Etobicoke" = runif(n = 24, min = 0, max = 1000),
-  )
+    "Number of Crimes in this Hour" = runif(n = 24, min = 0, max = 1000),
+  ) |> kable()
 simulated_data4
